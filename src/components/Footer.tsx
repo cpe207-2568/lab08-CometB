2c1667e103a040ca9450704f926464d054fea009
CometB <rpila23461@gmail.com>
2025-08-02 16:29:27 +0700

    finish

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import type { FooterProps } from "../libs/types";
+
+function Footer(props: FooterProps) {
+  return (
+    <footer className="text-secondary text-center p-2 bg-light mt-auto">
+      Copyright © {props.year} {props.fullName} {props.studentId}
+    </footer>
+  );
+}
+
+export default Footer;
